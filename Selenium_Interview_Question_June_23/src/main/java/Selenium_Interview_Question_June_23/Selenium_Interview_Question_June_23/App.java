c9ea4f1472097fd8913a5f3c1b46671d9c9aa7d8
shamb <shamb@LAPTOP-2GUJPCJO>
2021-06-23 17:41:49 +0530

    1st_commit_June23

---
+++ Selenium_Interview_Question_June_23/src/main/java/Selenium_Interview_Question_June_23/Selenium_Interview_Question_June_23/App.java
@@ -0,0 +1,7 @@
+package Selenium_Interview_Question_June_23.Selenium_Interview_Question_June_23;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
